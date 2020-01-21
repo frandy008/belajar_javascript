38a4806737addba4bff7b449c0fbe23fab536956
frandy008 <frandy008@users.noreply.github.com>
2020-01-21 20:50:18 +0800

    javascript
    Pengamanan dengan javascript v1

--- home.php
+++ home.php
@@ -41,10 +41,12 @@
           <?php  
           $sdp = mysql_query("SELECT * FROM tbl_pesanan");
           while ($data = mysql_fetch_array($sdp)) { 
+            $sd = mysql_query("SELECT * FROM tbl_pesanan WHERE id = '$data[id]'");
+            $dpid = mysql_fetch_array($sd);
           ?>
           <!-- Large modal -->
           <div class="modal fade bd-example-modal-lg<?= $data['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-dialog modal-md" role="document">
               <div class="modal-content">
                 <div class="modal-header">
                   <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
@@ -53,7 +55,111 @@
                   </button>
                 </div>
                 <div class="modal-body">
-                  
+                <form method="post" class="form-user">
+                  <input type="hidden" name="id" value="<?= $dpid['id'] ?>">
+                  <!-- Select Basic -->
+                  <div class="form-group">
+                    <div class="row">
+                      <label class="col-md-4 control-label" for="meja">Meja</label>
+                      <div class="col-md-8">
+                        <select id="meja" name="meja" class="form-control">
+                          <option value="">Pilih</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- Multiple Checkboxes -->
+                  <div class="form-group">
+                    <div class="row">
+                      <label class="col-md-4 control-label" for="minuman">Minuman</label>
+                      <div class="col-md-8">
+                      <div class="checkbox">
+                        <label for="minuman-0">
+                          <input type="checkbox" name="minuman[]" id="minuman-0" value="Kopi Hitam">
+                          Kopi Hitam
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label for="minuman-1">
+                          <input type="checkbox" name="minuman[]" id="minuman-1" value="Kopi Susu">
+                          Kopi Susu
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label for="minuman-2">
+                          <input type="checkbox" name="minuman[]" id="minuman-2" value="Teh Manis">
+                          Teh Manis
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label for="minuman-3">
+                          <input type="checkbox" name="minuman[]" id="minuman-3" value="Lemon Tea">
+                          Lemon Tea
+                        </label>
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- Multiple Checkboxes -->
+                  <div class="form-group">
+                    <div class="row">
+                      <label class="col-md-4 control-label" for="makanan">Makanan</label>
+                      <div class="col-md-8">
+                        <div class="checkbox">
+                          <label for="makanan-0">
+                            <input type="checkbox" name="makanan[]" id="makanan-0" value="Nasi Goreng Biasa">
+                            Nasi Goreng Biasa
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label for="makanan-1">
+                            <input type="checkbox" name="makanan[]" id="makanan-1" value="Nasi Goreng Spesial">
+                            Nasi Goreng Spesial
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label for="makanan-2">
+                            <input type="checkbox" name="makanan[]" id="makanan-2" value="Roti Bakar Coklat">
+                            Roti Bakar Coklat
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label for="makanan-3">
+                            <input type="checkbox" name="makanan[]" id="makanan-3" value="Roti Bakar Keju">
+                            Roti Bakar Keju
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- Textarea -->
+                  <div class="form-group">
+                    <div class="row">
+                      <label class="col-md-4 control-label" for="keterangan">Keterangan</label>
+                      <div class="col-md-8">                     
+                        <textarea class="form-control" id="keterangan" name="keterangan"><?= $dpid['keterangan'] ?></textarea>
+                      </div>
+                      
+                    </div>
+                  </div>
+
+                  <!-- Button -->
+                  <div class="form-group">
+                    <div class="row">
+                      <label class="col-md-4 control-label" for="input"></label>
+                      <div class="col-md-8">
+                        <button id="input" name="input" class="btn btn-primary">Input</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
                 </div>
               </div>
             </div>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-$('#footer').html(`
-		Copyright &copy; 2020 IMFDP
-	`);
+$('#footer').html(`Copyright &copy; 2020 IMFDP`);
+
+const footer = document.getElementById('footer');
+//const attFooterId = footer.getAttribute('id');
+
+const judulWeb = document.getElementsByTagName('h1')[0];
+
+if(footer == null){
+	console.log('Tolong jangan hapus id footer');
+}
+
+const attFooterHidden = footer.getAttribute('hidden');
+if(attFooterHidden == null){
+	console.log('Protection 1 Secure');
+}else{
+	judulWeb.innerHTML = 'Toko ini Pake Website Bajakan';
+	console.log('Hapus hidden pada footer');
+}
+
+const attFooterStyle = footer.getAttribute('style');
+if(attFooterStyle == null){
+	console.log('Protection 2 Secure');
+}else{
+	console.log('Hapus Style pada footer');
+}
